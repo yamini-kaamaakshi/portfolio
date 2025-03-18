4cfb679069032a4ea64f0548ca7764ce476ed866
Yamini <yaminikaamaakshi@gmail.com>
2025-03-18 18:43:00 +0530

    ui changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { ReactNode } from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import { ReactNode } from "react";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+
 export const metadata = {
     title: "portfolio",
     description: "A simple portfolio application built with Next.js 15",
 };
+
 const Layout = ({ children }: { children: ReactNode }) => {
     return (
-        <ClerkProvider>
-        <html lang="en">
-        <body className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-1 container mx-auto p-4">{children}</main>
-        <Footer />
-        </body>
-        </html>
+        <ClerkProvider
+            publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+        >
+            <html lang="en">
+            <body className="flex flex-col min-h-screen">
+            <Navbar />
+            <main className="flex-1 container mx-auto p-4">{children}</main>
+            <Footer />
+            </body>
+            </html>
         </ClerkProvider>
     );
 };
