c6223274e8f5924e31a844cc6724a96479da6687
Yamini <yaminikaamaakshi@gmail.com>
2025-03-19 15:51:49 +0530

    footer page updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,36 +3,36 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
-import {socialMediaLinks} from "@/lib/socialMediaLinks";
+import { socialMediaLinks } from "@/lib/socialMediaLinks";
 
 export default function Footer() {
     return (
-        <footer className="w-full bg-black dark:bg-black backdrop-blur-md shadow-md border-t border-gray-700 text-center p-6">
-            <nav className="flex justify-center space-x-6">
+        <footer className="w-full bg-black dark:bg-black backdrop-blur-md shadow-md border-t border-gray-700 text-center py-12 px-6">
+            <nav className="flex justify-center space-x-8">
                 <NavLink href="/" label="Home" />
                 <NavLink href="/about" label="About" />
                 <NavLink href="/projects" label="Projects" />
                 <NavLink href="/contact" label="Contact" />
             </nav>
 
-            <div className="mt-4 text-white text-sm">
+            <div className="mt-6 text-white text-xl font-semibold">
                 <p className="mt-2">Connect with me on:</p>
-                <div className="flex justify-center space-x-4 mt-2">
+                <div className="flex justify-center space-x-8 mt-3">
                     {Object.entries(socialMediaLinks).map(([platform, { url, icon }]) => (
                         <Link key={platform} href={url} target="_blank">
                             <Image
-                                width={50}
-                                height={50}
+                                width={70}
+                                height={70}
                                 src={icon}
                                 alt={platform}
-                                className="w-5 h-5"
+                                className="w-6 h-6 transition-transform duration-300 hover:scale-110"
                             />
                         </Link>
                     ))}
                 </div>
             </div>
 
-            <p className="mt-4 text-xs text-gray-400">
+            <p className="mt-6 text-lg text-gray-400">
                 © {new Date().getFullYear()} My Portfolio. All Rights Reserved.
             </p>
         </footer>
@@ -47,7 +47,7 @@ function NavLink({ href, label }: { href: string; label: string }) {
     return (
         <Link
             href={href}
-            className={`text-sm font-medium transition-colors duration-300 relative ${
+            className={`text-lg font-medium transition-colors duration-300 relative ${
                 isActive
                     ? "text-yellow-400 font-bold"
                     : "text-white hover:text-yellow-400"
